adee009b3cc68b87ef96a8537db2e8e77692a117
Elronnd <elronnd@elronnd.net>
2019-08-31 13:48:05 -0700

    Apparently ldc can't handle inlining assembly.  Remove for now (but I do want syscall() to be inline at some pointer)

--- src/syscaller.d
+++ src/syscaller.d
@@ -3,7 +3,7 @@ module syscaller;
 import plat_version;
 
 static if (plat_os == OS.Linux && plat_arch == Architecture.AMD64) {
-	extern (D) pragma(inline, true) long syscall(T...)(long which, T args) {
+	extern (D) long syscall(T...)(long which, T args) {
 		enum param_regs64 = ["RDI", "RSI", "RDX", "R10",  "R8",   "R9"];
 		enum param_regs32 = ["EDI", "ESI", "EDX", "R10D", "R8D", "R9D"];
 		enum param_regs16 = ["DI",  "SI",  "DX",  "R10W", "R8W", "R9W"];
