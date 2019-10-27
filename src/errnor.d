b46185807e1e8ed88f8f5ac3fbf8c55540f5ab38
Moonchild <elronnd@elronnd.net>
2019-10-26 23:14:13 -0700

    Add errno accessor functions that some software wants Only 70 more functions left to implement before druntime runs on top of it!

--- src/errnor.d
+++ src/errnor.d
@@ -2,7 +2,21 @@ module errnor;
 
 import plat_version;
 
-extern (C) __gshared int errno;
+extern (C):
+__gshared int errno;
+
+static if (plat_os == OS.Linux) {
+	int *__errno_location() {
+		return &errno;
+	}
+} else static if (plat_os == OS.FreeBSD) {
+	int *__error() {
+		return &errno;
+	}
+}
+
+// openbsd:
+//int *__errno();
 
 static if (plat_os == OS.Linux && plat_arch == Architecture.AMD64) {
 	enum {
